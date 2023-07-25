6b89fadb97e58f73ce3db22b79dcacc104a12b3a
liuwen2 <liuwen2@cmss.chinamobile.com>
2023-07-25 17:18:18 +0800

    引入admin监控组件

--- src/main/java/com/example/springboottools/SpringbootApplication.java
+++ src/main/java/com/example/springboottools/SpringbootApplication.java
@@ -1,10 +1,12 @@
 package com.example.springboottools;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@EnableAdminServer
 @SpringBootApplication
 @RestController
 public class SpringbootApplication {
